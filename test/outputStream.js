8253c6e94889b2f68bfe3d70946082086169c9ee
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-03-10 15:15:02 -0400

    Allow test stream labels. Works over and under highwatemark

--- test/outputStream.js
+++ test/outputStream.js
@@ -4,14 +4,17 @@ var inherits = require('inherits');
 inherits(LogStream, Writable);
 
 LogStream.prototype._write = function(chunk, enc, cb){
-  console.log("\nGOT CHUNK WITH LENGTH: %d\n", chunk.length);
+  console.log("\nSTREAM LABEL: %s", this._label);
+  console.log("\nGOT CHUNK WITH LENGTH: %d", chunk.length);
   console.log("\nCHUNK TYPE: %s", Buffer.isBuffer(chunk) && "Buffer" || typeof chunk);
   console.log(chunk);
+  console.log("\n");
   cb();
 }
 
-function LogStream(){
-  if(!(this instanceof LogStream)) return new LogStream();
+function LogStream(label){
+  if(!(this instanceof LogStream)) return new LogStream(label);
+  this._label = label;
   Writable.call(this);
 }
 
--- test/test.js
+++ test/test.js
@@ -2,4 +2,6 @@ var fs = require('fs');
 var lump = require('../index.js');
 var outputStream = require('./outputStream.js');
 
-fs.createReadStream('./cons.txt').pipe(lump(1024)).pipe(outputStream());
+fs.createReadStream('./cons.txt').pipe(lump(Math.pow(2,17))).pipe(outputStream("FIRST STREAM"));
+
+fs.createReadStream('./cons.txt').pipe(lump(0)).pipe(outputStream("SECOND STREAM"));
